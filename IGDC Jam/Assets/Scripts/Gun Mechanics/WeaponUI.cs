73f2875849ba70c41459a5b5aaeadc172e169cef
Supantha Paul <supapaul56@gmail.com>
2024-07-21 17:57:58 +0530

    ui changes

--- IGDC Jam/Assets/Scripts/Gun Mechanics/WeaponUI.cs
+++ IGDC Jam/Assets/Scripts/Gun Mechanics/WeaponUI.cs
@@ -5,13 +5,16 @@ public class WeaponUI : MonoBehaviour
 {
     [SerializeField] private WeaponsHolder weaponHolder; //would've used a service locator or a instance ideally
     [SerializeField] private TextMeshProUGUI ammoCountField;
-    [SerializeField] private GameObject reloadTextObject;
+    [SerializeField] private GameObject weaponUiObj;
 
     private void Update()
     {
-        if (weaponHolder.currentEquippedWeapon == null) return;
-
+        if (weaponHolder.currentEquippedWeapon == null)
+        {
+            weaponUiObj.SetActive(false);
+            return;
+        }
+        weaponUiObj.SetActive(true);
         ammoCountField.text = weaponHolder.currentEquippedWeapon.currentAmmo + "/" + weaponHolder.currentEquippedWeapon.stats.maxCapacity;
-        reloadTextObject.SetActive(weaponHolder.currentEquippedWeapon.IsReloading);
     }
 }
